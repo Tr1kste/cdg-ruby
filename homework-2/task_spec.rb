812461a9b4ecf8e41a83d8ce2e3060a509ca6c0f
Andrew Panko <trikster.seo@gmail.com>
2022-05-24 09:03:29 +0300

    refactoring code & spec

--- homework-2/task_spec.rb
+++ homework-2/task_spec.rb
@@ -1,28 +1,27 @@
-require 'rspec'
-require './task.rb'
+require "rspec"
+require "./task.rb"
 
-RSpec.describe "Tests for method 'string'" do
-    it "Text end's with CS" do
-        expect(string('renoCS')).to eq(64)
-        expect(string('reCS')).to eq(16)
-    end
-    it "Text doesn't end's with CS" do
-        expect(string('reno')).to eq('oner')
-        expect(string('hello')).to eq('olleh')
-        expect(string('CShello')).to eq('ollehSC')
-    end
-end
+RSpec.describe "task" do
+  it "#string: text end's with CS" do
+    expect(string("renoCS")).to eq(64)
+  end
 
-RSpec.describe "Tests for method 'pokemon'" do
-    it "Test for pokemon" do
-        num_pokemon = 3
-        result = [
-            {name: "Pika", color: "Yellow"},
-            {name: "Foo", color: "Green"},
-            {name: "Slow", color: "Red"}
-        ]
+  it "#string: Text doesn't end's with CS" do
+    expect(string("CShello")).to eq("ollehSC")
+  end
 
-        allow_any_instance_of(Object).to receive(:gets).and_return("Pika", "Yellow", "Foo", "Green", "Slow", "Red")
-        expect(pokemon(num_pokemon)).to eq(result)
-    end
+  it "#pokemon" do
+    allow_any_instance_of(Object).to receive(:gets).and_return("Pika", "Yellow", "Foo", "Green", "Slow", "Red")
+    result = [
+      { name: "Pika", color: "Yellow" },
+      { name: "Foo", color: "Green" },
+      { name: "Slow", color: "Red" },
+    ]
+
+    expect(pokemon(3)).to eq(result)
+  end
+
+  it "#pokemon: test type" do
+    expect(pokemon(4)).to be_an_instance_of(Array)
+  end
 end
