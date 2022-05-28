cbb7977da2b5120d0e03d555635dbe5b1290fe25
Andrew Panko <trikster.seo@gmail.com>
2022-05-28 15:03:04 +0300

    fix code

--- homework-3/task_1.rb
+++ homework-3/task_1.rb
@@ -1,50 +1,48 @@
-DATA_PATH = 'task.txt'
-BUFFER = 'buffer.txt'
+DATA_PATH = "task.txt"
+BUFFER = "buffer.txt"
 
 def index
-    File.foreach(DATA_PATH) { |line| puts line }
+  File.read(DATA_PATH)
 end
 
 def find(id)
-    File.foreach(DATA_PATH).with_index do |line, index|
+  File.foreach(DATA_PATH).with_index do |line, index|
     return line.chomp if index == id
-    end
+  end
 end
 
 def where(pattern)
-    arr = []
-    File.foreach(DATA_PATH).with_index do |string, index|
-        if string.include?(pattern)
-            arr.push(index)
-        end
+  arr = []
+  File.foreach(DATA_PATH).with_index do |string, index|
+    if string.include?(pattern)
+      arr.push(index)
     end
-    return arr.map {|i| puts i.to_i}
+  end
+  return arr.map { |i| puts i.to_i }
 end
 
 def update(id, text)
-    file = File.open(BUFFER, 'w')
+  file = File.open(BUFFER, "w")
 
-    File.foreach(DATA_PATH).with_index do |line, index|
-      file.puts(id == index ? text : line)
-    end
-  
-    file.close
-    File.write(DATA_PATH, File.read(BUFFER))
-  
-    File.delete(BUFFER) if File.exist?(BUFFER)
+  File.foreach(DATA_PATH).with_index do |line, index|
+    file.puts(id == index ? text : line)
+  end
+
+  file.close
+  File.write(DATA_PATH, File.read(BUFFER))
+
+  File.delete(BUFFER) if File.exist?(BUFFER)
 end
 
 def delete(id)
-    file = File.open(BUFFER, 'w')
+  file = File.open(BUFFER, "w")
 
-    File.foreach(DATA_PATH).with_index do |line, index|
-      file.puts line if index != id
-    end
-  
-    file.close
-    File.write(DATA_PATH, File.read(BUFFER))
-  
-    File.delete(BUFFER) if File.exist?(BUFFER)
+  File.foreach(DATA_PATH).with_index do |line, index|
+    file.puts line if index != id
+  end
+
+  file.close
+  File.write(DATA_PATH, File.read(BUFFER))
+
+  File.delete(BUFFER) if File.exist?(BUFFER)
 end
-
-index
--- homework-3/task_1_spec.rb
+++ homework-3/task_1_spec.rb
@@ -1,27 +1,18 @@
-require 'rspec'
-require './task_1.rb'
-  
-RSpec.describe 'Methods' do
+require "rspec"
+require "./task_1.rb"
 
-    let(:where_out) do
-<<-OUTPUT
-1
-2
-OUTPUT
-    end
+RSpec.describe "file methods" do
+  let(:file) { File.read(DATA_PATH) }
 
-    let(:file) {File.open(DATA_PATH)}
+  it "#index" do
+    expect(index).to eq(file)
+  end
 
-    it '#index' do
-        expect{index}.to output(file.read).to_stdout
-    end
+  it "#find" do
+    expect(find(3)).to eq("Роман Вечнов 35")
+  end
 
-    it '#find' do
-        expect(find(3)).to eq('Роман Вечнов 35')
-    end
-
-    it '#where' do
-        expect{where('22')}.to output(where_out).to_stdout
-    end
-
+  it "#where" do
+    expect { where("22") }.to output("1\n2\n").to_stdout
+  end
 end
